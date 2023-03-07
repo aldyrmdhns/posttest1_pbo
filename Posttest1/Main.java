fbaeacea349217388e8fe94e9de41ba7ff7cd674
aldyrmdhns <92864319+aldyrmdhns@users.noreply.github.com>
2023-03-07 09:51:07 +0800

    Add files via upload

--- Posttest1/Main.java
+++ Posttest1/Main.java
@@ -8,7 +8,12 @@ public class Main{
     //Static Declaration for Input and ArrayList
     static InputStreamReader masuk = new InputStreamReader(System.in);
     static BufferedReader masukan = new BufferedReader(masuk);
-    static ArrayList<Order> pesan = new ArrayList<>();
+    static ArrayList<Order> pesan = new ArrayList<>(){
+        {
+            add(new Order("Takami Izaya", "081234567890", "Jalan Perjuangan 1", "Pintu Besar", 101.2, 192.3, 2));
+            add(new Order("Sakayanagi Ruika", "08098765432", "Jalan Pertempuran 1", "Jendela Kecil", 80.78, 102.3, 5));
+        }
+    };
 
     //Main Class
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -35,7 +40,8 @@ public class Main{
                 } catch (Exception e) {
                     ClearScreen();
                     System.out.println(" Masukan Input dengan Benar ! ! !");
-                    Wait(1000);
+                    System.out.println("  Tekan Enter Untuk Kembali");
+                    masukan.readLine();
                 }
             }
             
@@ -64,7 +70,7 @@ public class Main{
                 default:
                     ClearScreen();
                     System.out.println("- Inputan Salah -");
-                    Wait(1000);
+                    masukan.readLine();
                     break;
             }
         }
@@ -76,11 +82,8 @@ public class Main{
         if (isiArray == 0) {
             System.out.println(" Data Pesanan Masih Kosong ");
         } else{
-            System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------");
-            System.out.println("|         Nama            |       No Hp       |           Alamat           |        Barang        |     Panjang     |     Tinggi     |     Banyak     |");
-            System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------");
             for (int i = 0; i < isiArray; i++) {
-                pesan.get(i).isDisplay();
+                pesan.get(i).isDisplay(); System.out.println();
             }
         }
         System.out.println("Tekan enter untuk melanjutkan"); masukan.readLine();
@@ -92,114 +95,193 @@ public class Main{
         int banyak;
         ClearScreen();
 
-        System.out.println("================================================================");
-        System.out.println("|                      Tambah Data Pesanan                     |");
-        System.out.println("================================================================");
-        System.out.print(" Nama Pemesan           : "); nama = masukan.readLine();
-        System.out.print(" No Hp Pemesan          : "); noHp = masukan.readLine();
-        System.out.print(" Alamat Pemesan         : "); alamat = masukan.readLine();
-        System.out.print(" Nama Barang Pesanan    : "); barang = masukan.readLine();
-        System.out.print(" Panjang Barang         : "); panjang = Double.parseDouble(masukan.readLine());
-        System.out.print(" Tinggi Barang          : "); tinggi = Double.parseDouble(masukan.readLine());
-        System.out.print(" Banyak Barang          : "); banyak = Integer.parseInt(masukan.readLine());
+        while (true) {
+            try {
+                System.out.println("================================================================");
+                System.out.println("|                      Tambah Data Pesanan                     |");
+                System.out.println("================================================================");
+                System.out.print(" Nama Pemesan           : "); nama = masukan.readLine();
+                System.out.print(" No Hp Pemesan          : "); noHp = masukan.readLine();
+                System.out.print(" Alamat Pemesan         : "); alamat = masukan.readLine();
+                System.out.print(" Nama Barang Pesanan    : "); barang = masukan.readLine();
+                System.out.print(" Panjang Barang         : "); panjang = Double.parseDouble(masukan.readLine());
+                System.out.print(" Tinggi Barang          : "); tinggi = Double.parseDouble(masukan.readLine());
+                System.out.print(" Banyak Barang          : "); banyak = Integer.parseInt(masukan.readLine());
+                break;    
+            } catch (Exception e) {
+                ClearScreen();
+                System.out.println(" Inputan Anda Salah, Mohon Masukan dengan Benar");
+                System.out.println("  Tekan Enter untuk Mengulangi");
+                masukan.readLine();
+                ClearScreen();
+            }
+
+        }
         System.out.println("================================================================");
 
         Order addOrder = new Order(nama, noHp, alamat, barang, panjang, tinggi, banyak);
         pesan.add(addOrder);
         System.out.println("                   Pesanan Berhasil Ditambahkan                   ");
         System.out.println("================================================================");
-        Wait(1000);
+        System.out.println("  Tekan Enter Untuk Kembali");
+        masukan.readLine();;
     }
 
     public static void isChange() throws NumberFormatException, IOException{
-        System.out.println("================================================================");
-        System.out.println("|                       Ubah Data Pesanan                      |");
-        System.out.println("================================================================");
-        System.out.print("  Masukan Nama Pemesan : "); String cariNama = masukan.readLine();
-        System.out.println("================================================================");
-        for (Order order : pesan) {
-            if (order.nama.equals(cariNama)) {
-                System.out.println("  1. Ubah Nama\t\t\t  2. Ubah No Hp\n  3. Ubah Alamat\t\t  4. Ubah Barang\n  5. Ubah Panjang\t\t  6. Ubah Tinggi\n  7. Ubah Banyak Barang\t\t  0. Kembali ke menu Sebelumnya");
-                System.out.println("================================================================");
-                System.out.print("  >> "); int pilihan = Integer.parseInt(masukan.readLine());
-                
-                switch (pilihan) {
-                    case 1:
-                        System.out.print("  Masukan Nama Pemesan Yang Baru : "); String newName = masukan.readLine();
-                        order.setNama(newName);
-                        System.out.println("\n  Nama Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 2:
-                        System.out.print("  Masukan No Hp Yang Baru : "); String newNohp = masukan.readLine();
-                        order.setNoHp(newNohp);
-                        System.out.println("\n  No Hp Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 3:
-                        System.out.print("  Masukan Alamat Yang Baru : "); String newAlamat = masukan.readLine();
-                        order.setAlamat(newAlamat);
-                        System.out.println("\n  Alamat Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 4:
-                        System.out.print("  Masukan Nama barang Yang Baru : "); String newBarang = masukan.readLine();
-                        order.setBarang(newBarang);
-                        System.out.println("\n  Nama barang Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 5:
-                        System.out.print("  Masukan Panjang Barang Yang Baru : "); Double newPanjang = Double.parseDouble(masukan.readLine());
-                        order.setPanjang(newPanjang);
-                        System.out.println("\n  Panjang Barang Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 6:
-                        System.out.print("  Masukan Tinggi Barang Yang Baru : "); Double newTinggi = Double.parseDouble(masukan.readLine());
-                        order.setTinggi(newTinggi);
-                        System.out.println("\n  Tinggi Barang Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 7:
-                        System.out.print("  Masukan Banyak Barang Yang Baru : "); int newBanyak = Integer.parseInt(masukan.readLine());
-                        order.setBanyak(newBanyak);
-                        System.out.println("\n  Banyak Barang Berhasil Diubah!");
-                        System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
-                        break;
-                    case 0:
-                        break;
-                    default:
-                        System.out.println("  Inputan Tidak Tersedia ");
-                        break;
+        int isiArray = pesan.size();
+        if (isiArray == 0) {
+            System.out.println(" Data Pesanan Masih Kosong ");
+        } else {
+            System.out.println("================================================================");
+            System.out.println("|                       Ubah Data Pesanan                      |");
+            System.out.println("================================================================");
+            for (int i = 0; i < pesan.size(); i++) {
+                pesan.get(i).isDisplayNama();
+            }
+            System.out.println("================================================================");
+            System.out.print("  Masukan Nama Pemesan : "); String cariNama = masukan.readLine();
+            System.out.println("================================================================");
+            for (Order order : pesan) {
+                if (order.nama.equals(cariNama)) {
+                    System.out.println("  1. Ubah Nama\t\t\t  2. Ubah No Hp\n  3. Ubah Alamat\t\t  4. Ubah Barang\n  5. Ubah Panjang\t\t  6. Ubah Tinggi\n  7. Ubah Banyak Barang\t\t  0. Kembali ke menu Sebelumnya");
+                    System.out.println("================================================================");
+                    System.out.print("  >> "); int pilihan = Integer.parseInt(masukan.readLine());
+                    
+                    switch (pilihan) {
+                        case 1:
+                            System.out.print("  Masukan Nama Pemesan Yang Baru : "); String newName = masukan.readLine();
+                            order.setNama(newName);
+                            System.out.println("\n  Nama Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 2:
+                            System.out.print("  Masukan No Hp Yang Baru : "); String newNohp = masukan.readLine();
+                            order.setNoHp(newNohp);
+                            System.out.println("\n  No Hp Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 3:
+                            System.out.print("  Masukan Alamat Yang Baru : "); String newAlamat = masukan.readLine();
+                            order.setAlamat(newAlamat);
+                            System.out.println("\n  Alamat Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 4:
+                            System.out.print("  Masukan Nama barang Yang Baru : "); String newBarang = masukan.readLine();
+                            order.setBarang(newBarang);
+                            System.out.println("\n  Nama barang Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 5:
+                            Double newPanjang = null;
+                            while (true) {
+                                try {
+                                    System.out.print("  Masukan Panjang Barang Yang Baru : "); newPanjang = Double.parseDouble(masukan.readLine());
+                                    break;
+                                } catch (Exception e) {
+                                    System.out.println(" Inputan Anda Salah, Mohon Masukan dengan Benar");
+                                    System.out.println("  Tekan Enter untuk Mengulangi");
+                                    masukan.readLine();
+                                }
+                            }
+                            order.setPanjang(newPanjang);
+                            System.out.println("\n  Panjang Barang Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 6:
+                            Double newTinggi = null;
+                            while (true) {
+                                try {
+                                    System.out.print("  Masukan Tinggi Barang Yang Baru : "); newTinggi = Double.parseDouble(masukan.readLine());
+                                    break;
+                                } catch (Exception e) {
+                                    System.out.println(" Inputan Anda Salah, Mohon Masukan dengan Benar");
+                                    System.out.println("  Tekan Enter untuk Mengulangi");
+                                    masukan.readLine();
+                                }
+                                
+                            }
+                            order.setTinggi(newTinggi);
+                            System.out.println("\n  Tinggi Barang Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 7:
+                            int newBanyak = 0;
+                            while (true) {
+                                try {
+                                    System.out.print("  Masukan Banyak Barang Yang Baru : "); newBanyak = Integer.parseInt(masukan.readLine());
+                                    break;
+                                } catch (Exception e) {
+                                    System.out.println(" Inputan Anda Salah, Mohon Masukan dengan Benar");
+                                    System.out.println("  Tekan Enter untuk Mengulangi");
+                                    masukan.readLine();
+                                }
+                            }
+                            order.setBanyak(newBanyak);
+                            System.out.println("\n  Banyak Barang Berhasil Diubah!");
+                            System.out.println("\n  Tekan Enter untuk Kembali!");masukan.readLine();
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("  Inputan Tidak Tersedia ");
+                            break;
+                    }
                 }
             }
         }
-
     }
 
     public static void isDelete() throws IOException{
-        System.out.println("================================================================");
-        System.out.println("|                       Ubah Data Pesanan                      |");
-        System.out.println("================================================================");
-        System.out.print("  Masukan Nama Pemesan : "); String cariNama = masukan.readLine();
-        System.out.println("================================================================");
-        for (int i = 0; i < pesan.size(); i++) {
-            if (pesan.get(i).nama.equals(cariNama)) {
-                pesan.remove(i);
-                System.out.println("Pesanan Telah Dihapus"); masukan.readLine();
+        int isiArray = pesan.size();
+        if (isiArray == 0) {
+            System.out.println(" Data Pesanan Masih Kosong ");
+        } else {
+            System.out.println("================================================================");
+            System.out.println("|                      Hapus Data Pesanan                      |");
+            System.out.println("================================================================");
+            for (int i = 0; i < pesan.size(); i++) {
+                pesan.get(i).isDisplayNama();
             }
+            System.out.println("================================================================");
+            System.out.print("  Masukan Nama Pemesan : "); String cariNama = masukan.readLine();
+            System.out.println("================================================================");
+            for (int i = 0; i < pesan.size(); i++) {
+                if (pesan.get(i).nama.equals(cariNama)) {
+                    pesan.get(i).isDisplay();
+                    while (true) {
+                        System.out.println(" Apakah Anda Yakin untuk Menghapus? [y/n]"); char acc = (char)masukan.read();
+                        if (acc == 'y') {
+                            pesan.remove(i);
+                            System.out.println("Pesanan Telah Dihapus");
+                            System.out.println("  Tekan Enter Untuk Kembali");
+                            masukan.readLine();
+                            break;
+                        } else if(acc == 'n'){
+                            System.out.println("Tidak Jadi Menghapus Pesanan");
+                            System.out.println("  Tekan Enter Untuk Kembali");
+                            masukan.readLine();
+                            break;
+                        } else{
+                            System.out.println(" Inputan tidak Tersedia");
+                            System.out.println("  Tekan Enter Untuk Kembali");
+                            masukan.readLine();
+                        }
+                    }
+                }
+        }
     }
 }
 
     //Some Other Methods/Function
-    public static void Wait(int lama){
-        try{
-            Thread.sleep(lama);
-        }
-        catch(InterruptedException ex){
-            Thread.currentThread().interrupt();
-        }
-    }
+    // public static void Wait(int lama){
+    //     try{
+    //         Thread.sleep(lama);
+    //     }
+    //     catch(InterruptedException ex){
+    //         Thread.currentThread().interrupt();
+    //     }
+    // }
 
     public static void ClearScreen() {
         try {
--- Posttest1/Order.java
+++ Posttest1/Order.java
@@ -14,7 +14,19 @@ public class Order {
     };
 
     public void isDisplay(){
-        System.out.println("| " + this.nama + "          " + this.noHp + "        " + this.alamat + "            " + this.barang + "         " + this.panjang + "     " + this.tinggi + "     " + this.banyak + "    |");
+        System.out.println("=====================================");
+        System.out.println("| Nama Pemesan      : " + this.nama);
+        System.out.println("| No HP Pemesan     : " + this.noHp);
+        System.out.println("| Alamat Pemesan    : " + this.alamat);
+        System.out.println("| Barang Pesanan    : " + this.barang);
+        System.out.println("| Panjang Barang    : " + this.panjang);
+        System.out.println("| Tinggi Barang     : " + this.tinggi);
+        System.out.println("| Banyak Barang     : " + this.banyak);
+        System.out.println("=====================================");
+    }
+
+    public void isDisplayNama() {
+        System.out.println(" - " + this.nama);
     }
 
     public void setNama(String isNama) {        
